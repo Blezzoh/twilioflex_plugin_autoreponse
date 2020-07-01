19b66ec7a3732575fee3c38c20b265655cdea5bc
blezzoh <iradub0@sewanee.edu>
2020-07-01 13:13:44 -0700

    plugin canned messages

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+// canned message than an agent can choose from 
+export const cannedMessages = [
+    'Canned Response',
+    'Canned Response 2',
+    'Canned Response 3',
+    'laura ipsum Response'
+]
+// starts after saying Hi, I am first lastname <firstAgentAutoResponse>
+export const firstAgentAutoResponse = 'From Professional Credit Services.'
